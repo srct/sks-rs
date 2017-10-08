c9429438087a1aeeeb8c2d7c88c21609812c2cc3
Kunal Sarkhel <ksarkhel@gmu.edu>
2017-10-08 01:55:09 -0400

    Read YAML from config.yml

--- src/bin.rs
+++ src/bin.rs
@@ -2,14 +2,17 @@ extern crate yaml_rust;
 extern crate sks_app_lib;
 
 use yaml_rust::{Yaml, YamlLoader};
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    let s: &str = "
----
-hkp_address: 0.0.0.0
-hkp_port: 11371
-";
-    let docs: Vec<Yaml> = YamlLoader::load_from_str(s).unwrap();
+    let filename = "config.yml";
+    let mut f = File::open(filename).expect(&format!("Config file {} not found", filename));
+
+    let mut s = String::new();
+    f.read_to_string(&mut s).expect("Unable to read config file");
+
+    let docs: Vec<Yaml> = YamlLoader::load_from_str(&s).unwrap();
 
     // We only care about the first document
     let doc: &Yaml = &docs[0];
